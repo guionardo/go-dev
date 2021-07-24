7de2d580763d4a9b8f5f3397761bbf572d716a94
Guionardo Furlan <guionardo.furlan@ambevtech.com.br>
2021-07-24 15:36:17 -0300

    Added install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-export DOCKER_BUILDKIT=1
-@docker build . --target bin --output bin/ --platform local
-bin/go-dev install
+if [ -x "$(command -v docker)" ]; then
+  export DOCKER_BUILDKIT=1
+  docker build https://github.com/guionardo/go-dev.git#develop --target bin --output bin/ --platform local
+  bin/go-dev install
+  bin/go-dev help
+  echo "Open a new console to load updated configurations"
+  echo "Usage: dev <options>"
+else
+  echo "Docker is not installed"
+fi
